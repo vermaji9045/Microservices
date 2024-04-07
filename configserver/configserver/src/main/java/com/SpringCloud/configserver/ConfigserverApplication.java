e25053904147d655bca5578068cdbe8d03ea295e
vermaji9045 <59643845+vermaji9045@users.noreply.github.com>
2024-04-07 17:41:05 +0530

    Add files via upload

---
+++ configserver/configserver/src/main/java/com/SpringCloud/configserver/ConfigserverApplication.java
@@ -0,0 +1,17 @@
+package com.SpringCloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
