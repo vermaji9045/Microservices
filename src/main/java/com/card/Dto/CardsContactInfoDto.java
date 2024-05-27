781e33d0bf1db446b501d7158e13917e4d777971
vermaji9045 <59643845+vermaji9045@users.noreply.github.com>
2024-05-27 13:54:56 +0530

    All the changes have been updated

--- src/main/java/com/card/Dto/CardsContactInfoDto.java
+++ src/main/java/com/card/Dto/CardsContactInfoDto.java
@@ -2,12 +2,20 @@ package com.card.Dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String,String>contactDetails, List<String> onContactSupport)
-{
+@Getter
+@Setter
 
+@ConfigurationProperties(prefix = "cards")
+public class CardsContactInfoDto
+{
+    private String message;
+    private  Map<String,String>contactDetails;
+    private  List<String> onContactSupport;
 
 }
